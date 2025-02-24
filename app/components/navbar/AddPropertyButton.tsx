63f20f71e39c65c403c9a0bf20757ac8b87b6829
Derek Eduardo Aguilar Reyes <aguilarrde753@gmail.com>
2025-02-24 14:27:22 -0600

    02 Code for nav bar

---
+++ app/components/navbar/AddPropertyButton.tsx
@@ -0,0 +1,9 @@
+const AddPropertyButton=()=>{
+    return (
+        <div className="p1 lg:p-2 cursor-pointer text-sm font-semibold rounded-full hover:bg-gray-200">
+            Pon tu casa en Djangobnb
+        </div>
+    )
+}
+
+export default AddPropertyButton;
