2c4e50e781e10bb8e9f70db65e102ffd7de16175
Derek Eduardo Aguilar Reyes <aguilarrde753@gmail.com>
2025-02-25 11:40:49 -0600

    06 Host page (static)

---
+++ app/components/ContactButton.tsx
@@ -0,0 +1,9 @@
+const ContactButton=()=>{
+    return (
+        <div className="mt-6 py-4 px-6 cursor-pointer bg-airbnb hover:bg-airbnbDark transition text-white rounded-xl">
+            Escríbele al anfitrión
+        </div>
+    );
+}
+
+export default ContactButton;
