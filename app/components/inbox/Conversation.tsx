ccafca54fe2a456636ae506dea82b25ebad537dc
Derek Eduardo Aguilar Reyes <aguilarrde753@gmail.com>
2025-02-26 11:59:51 -0600

    09 Inbox page (static)

---
+++ app/components/inbox/Conversation.tsx
@@ -0,0 +1,12 @@
+const Conversation=()=>{
+    return(
+        <div className="px-6 py-4 border cursor-pointer border-gray-300 rounded-xl">            
+            <p className="mb-6 text-xl">
+                John Doe
+            </p>
+            <p className=" text-airbnbDark">Ver conversación</p>
+        </div>
+    )
+}
+
+export default Conversation;
