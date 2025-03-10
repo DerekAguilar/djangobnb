fb6fc842805d03c9565ff36a6b1a979ab087d859
Derek Eduardo Aguilar Reyes <aguilarrde753@gmail.com>
2025-03-10 17:59:37 -0600

    22 Implement authentication: Sign up

--- backend/djangobnb_backend/djangobnb_backend/settings.py
+++ backend/djangobnb_backend/djangobnb_backend/settings.py
@@ -24,13 +24,13 @@ SITE_ID=1
 WEBSITE_URL='http://localhost:8000'
 
 SIMPLE_JWT={
-    'ACCESS_TOKEN_LIFETIME':timedelta(minutes=60),
-    'REFRESH_TOKEN_LIFETIME':timedelta(days=7),
-    'ROTATE_REFRESH_TOKEN':False,
-    'BLACKLIST_AFTER_ROTATION':False,
-    'UPDATE_LAST_LOGIN':True,
-    'SIGNING_KEY':'acomplexkey',
-    'ALGORITHM':'HS512',
+    "ACCESS_TOKEN_LIFETIME":timedelta(minutes=60),
+    "REFRESH_TOKEN_LIFETIME":timedelta(days=7),
+    "ROTATE_REFRESH_TOKEN":False,
+    "BLACKLIST_AFTER_ROTATION":False,
+    "UPDATE_LAST_LOGIN":True,
+    "SIGNING_KEY":"acomplexkey",
+    "ALGORITHM":"HS512",
 }
 
 ACCOUNT_USER_MODEL_USERNAME_FIELD=None
@@ -73,6 +73,8 @@ INSTALLED_APPS = [
     'rest_framework',
     'rest_framework.authtoken',
     'rest_framework_simplejwt',
+
+    'allauth',
     'allauth.account',
     
     'dj_rest_auth',
--- djangobnb/app/components/modals/SignupModal.tsx
+++ djangobnb/app/components/modals/SignupModal.tsx
@@ -3,25 +3,70 @@
 import CustomButton from "../forms/CustomButton";
 import Modal from "./Modal";
 import {useState} from "react";
+import { useRouter } from "next/navigation";
 import useSignupModal from "@/app/hooks/useSignupModal";
+import apiService from "@/app/services/apiService";
+import { handleLogin } from "@/app/lib/actions";
 
 const SignupModal=()=>{
+    //
+    // Variables
+
+    const router =useRouter();
     const signupModal=useSignupModal();
+    const [email,setEmail]=useState('');
+    const [password1,setPassword1]=useState('');
+    const [password2,setPassword2]=useState('');
+    const [errors,setErrors]=useState<string[]>([]);
+    //
+    // Función subir
+
+    const submitSignup=async ()=>{
+        const formData={
+            email:email,
+            password1:password1,
+            password2:password2
+        }
+        const response=await apiService.post('/api/auth/register/',JSON.stringify(formData));
+
+        if(response.access) {
+            handleLogin(response.user.pk,response.access,response.refresh);
+            signupModal.close();
+            router.push('/');
+        } else {
+            const tmpErrors:string[]=Object.values(response).map((error:any)=>{
+                return error;
+            })
+            setErrors(tmpErrors);
+        }
+    }
+
     const content= (
         <>
             <h2 className="mb-6 text-2xl">
                 ¡Te damos la bienvenida a Djangobnb!
             </h2>
-            <form className="space-y-4">
-                <input placeholder="Correo electrónico" type="email" className="w-full h-[54px] px-4 border border-gray-300 rounded-xl"/>
-                <input placeholder="Contraseña" type="password" className="w-full h-[54px] px-4 border border-gray-300 rounded-xl"/>
-                <input placeholder="Confirmar contraseña" type="password" className="w-full h-[54px] px-4 border border-gray-300 rounded-xl"/>
-                <div className="p-5 bg-airbnb text-white rounded-xl opacity-80">
-                    Mensaje de error
-                </div>
+            <form 
+            className="space-y-4"
+            action={submitSignup}
+            >
+                <input onChange={(e) => setEmail(e.target.value)} placeholder="Correo electrónico" type="email" className="w-full h-[54px] px-4 border border-gray-300 rounded-xl"/>
+                <input onChange={(e) => setPassword1(e.target.value)} placeholder="Contraseña" type="password" className="w-full h-[54px] px-4 border border-gray-300 rounded-xl"/>
+                <input onChange={(e) => setPassword2(e.target.value)} placeholder="Confirmar contraseña" type="password" className="w-full h-[54px] px-4 border border-gray-300 rounded-xl"/>
+                {errors.map((error,index)=> {
+                    return (
+                        <div 
+                        className="p-5 bg-airbnb text-white rounded-xl opacity-80" 
+                        key={`error_${index}`}
+                        >
+                            {error}
+                        </div>
+                    )
+                })}
+                
                 <CustomButton
                 label="Continuar"
-                onClick={()=> console.log('Crear cuenta')}
+                onClick={submitSignup}
                 />
             </form>
         </>
