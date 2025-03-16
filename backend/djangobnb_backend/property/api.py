bb92d896529bebdbb931b16cc84c3293e6c6d14a
Derek Eduardo Aguilar Reyes <aguilarrde753@gmail.com>
2025-03-16 16:57:55 -0600

    31 Implemented Set property as favorite

--- backend/djangobnb_backend/property/api.py
+++ backend/djangobnb_backend/property/api.py
@@ -1,15 +1,30 @@
 from django.http import JsonResponse
 
 from rest_framework.decorators import api_view,authentication_classes,permission_classes
+from rest_framework_simplejwt.tokens import AccessToken
 
 from .forms import PropertyForm
 from .models import Property,Reservation
 from .serializers import PropertiesListSerializer,PropertiesDetailsSerializer,ReservationsListSerializer
+from useraccount.models import User
 
 @api_view(['GET'])
 @authentication_classes([])
 @permission_classes([])
 def properties_list(request):
+    # Auth
+    try:
+        token=request.META['HTTP_AUTHORIZATION'].split('Bearer ')[1]
+        token=AccessToken(token)
+        user_id=token.payload['user_id']
+        user=User.objects.get(pk=user_id)
+    except Exception as e:
+        user=None
+
+    print('user',user)
+    #
+
+    favorites=[]
     properties=Property.objects.all()
 
     # Filtrar por anfitrión
@@ -18,10 +33,20 @@ def properties_list(request):
         properties=properties.filter(host_id=host_id)
     #
 
+    # Favoritos
+    if user:
+        for property in properties:
+            if user in property.favorited.all():
+                favorites.append(property.id)
+    print('favorites',favorites)
+    #
+
     serializer=PropertiesListSerializer(properties,many=True)
 
     return JsonResponse({
-        'data':serializer.data
+        'data':serializer.data,
+        'favorites':favorites
+
     })
 
 @api_view(['GET'])
@@ -83,3 +108,15 @@ def book_property(request,pk):
     except Exception as e:
         print('Error',e)
         return JsonResponse({'success':False})
+    
+@api_view(['POST'])
+def toggle_favorite(request,pk):
+    property=Property.objects.get(pk=pk)
+    if request.user in property.favorited.all():
+        property.favorited.remove(request.user)
+        
+        return JsonResponse({'is_favorite':False})
+    else:
+        property.favorited.add(request.user)
+
+        return JsonResponse({'is_favorite':True})
--- djangobnb/app/components/properties/PropertyListItem.tsx
+++ djangobnb/app/components/properties/PropertyListItem.tsx
@@ -1,15 +1,19 @@
 import Image from "next/image";
 import { PropertyType } from "./PropertyList";
 import { useRouter } from "next/navigation";
+import FavoriteButton from "../FavoriteButton";
 
 interface PropertyProps {
-    property: PropertyType
+    property: PropertyType;
+    markFavorite?:(isfavorite:boolean)=>void;
 }
 
 const PropertyListItem:React.FC<PropertyProps>=({
-    property
+    property,
+    markFavorite
 })=>{
     const router=useRouter();
+
     return (
         <div 
         className="cursor-pointer"
@@ -23,6 +27,14 @@ const PropertyListItem:React.FC<PropertyProps>=({
                 className="hover:scale-110 object-cover transition h-full w-full"
                 alt="Beach house"
                 />
+
+                {markFavorite && (
+                    <FavoriteButton
+                    id={property.id}
+                    is_favorite={property.is_favorite}
+                    markFavorite={(is_favorite)=>markFavorite(is_favorite)}
+                    />
+                )}
             </div>
             <div className="mt-2">
                 <p className="text-lg font-bold">
